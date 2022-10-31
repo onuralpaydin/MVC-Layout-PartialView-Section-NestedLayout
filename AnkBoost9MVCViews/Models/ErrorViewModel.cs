11b1b99754eb84809a85ed6e5410751a8353a832
Onur Alp Aydın <o.a.aydin17@gmail.com>
2022-10-31 15:54:22 +0300

    Add project files.

---
+++ AnkBoost9MVCViews/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AnkBoost9MVCViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
